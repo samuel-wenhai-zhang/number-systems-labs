fd3763581d390daa748cb7983b9ec3b2b0dcc939
Chad Purdy <cpurdy@flourbluffschools.net>
2023-10-11 12:14:21 -0500

    Add files via upload

--- Lab3/NumberRunner.java
+++ Lab3/NumberRunner.java
@@ -1,19 +1,36 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-
 //Name -
- 
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
-import java.util.ArrayList;
 import static java.lang.System.*;
 
 public class NumberRunner
 {
 	public static void main ( String[] args ) throws IOException
 	{
-		//add test cases
+	   ArrayList<Number> numList = new ArrayList<Number>();
+
+		Scanner file = new Scanner(new File("number.dat"));
+
+		int runs = file.nextInt();
+		file.nextLine();
+		for(int i=0; i<runs;i++)
+		{
+			String num = file.next();
+			int base = file.nextInt();
+			numList.add(new Number(num,base));
+		}
+
+		Collections.sort(numList);
+
+		for(Number n : numList)
+		{
+			out.println(n);
+		}
 	}
 }
