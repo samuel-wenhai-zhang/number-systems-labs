1e8724afa9eedfc4a31127432cdccdfc981f7704
Boltcrasher <Boltcrasher@gmail.com>
2023-10-11 13:31:53 -0500

    done 3

--- Lab3/Number.java
+++ Lab3/Number.java
@@ -1,25 +1,50 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Samuel Zhang
 
 import static java.lang.System.*;
 
 public class Number implements Comparable<Number>
 {
 
-	//add in instance variables
-	
-	//add in a constructor or two
+	private String number;
+	private int base;
+	private String binary;
+	private int numOnes;
+
+	public Number() {
+	}
+
+	public Number(String number, int base) {
+		this.number = number;
+		this.base = base;
+		binary = Integer.toBinaryString(Integer.parseInt(number, base));
+		numOnes = 0;
+		for (int i = 0; i < binary.length(); i++) {
+			if (binary.charAt(i) == '1') {
+				numOnes++;
+			}
+		}
+	}
+
 
 
 	public int compareTo(Number param)
 	{
-		return 0;		
+		if (numOnes != param.numOnes) {
+			return numOnes - param.numOnes;
+		}
+		else if (Integer.parseInt(number, base) != Integer.parseInt(param.number, param.base)) {
+			return Integer.parseInt(number, base) - Integer.parseInt(param.number, param.base);
+		}
+		else {
+			return number.compareTo(param.number);
+		}
 	}
 	
 	public String toString( )
 	{
-	  return "";
+	  return number + " " + Integer.parseInt(number, base) + " " + binary;
 	}
 }
