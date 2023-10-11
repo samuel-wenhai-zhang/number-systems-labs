c5df388942bd8884c7b04de8d120db6d46eeb329
Boltcrasher <Boltcrasher@gmail.com>
2023-10-11 13:06:48 -0500

    Update BaseConversion.java

--- Lab1/BaseConversion.java
+++ Lab1/BaseConversion.java
@@ -26,21 +26,34 @@ public class BaseConversion
 	{
 		int base10 = 0;
 		for (int i = 0; i < number.length(); i++) {
-			int digit = (int) number.charAt(i);
-			base10 += digit * Math.pow(base, number.length() - 1 - i);
+			int digit = number.charAt(i);
+			if (digit >= 65 && digit <= 90) {
+				digit -= 55;
+			}
+			else {
+				digit -= 48;
+			}
+			base10 += digit *  Math.pow(base, number.length() - 1 - i);
 		}
-
 		return base10;
 	}
 
 	public String getNum(int newBase)
 	{
-	   int base10 = convertToTen();
-		String newNum="";
-
+		int base10 = convertToTen();
+		String newNum = "";
 		while (base10 > 0) {
-			newNum += base10 % newBase;
+			int remainder = base10 % newBase;
+
+			if (remainder >= 10) {
+				char letter = (char) (remainder + 55);
+				newNum = letter + newNum;
+			}
+			else {
+				newNum = remainder + newNum;
+			}
 			
+			base10 /= newBase;
 		}
 
 		return newNum+"-"+newBase;
